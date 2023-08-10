268b246e545590d833fcff3e2f2bcbf4b503a702
DanielAros <danielaros200@gmail.com>
2023-08-10 13:11:36 -0600

    mi chamba x3

--- src/Escena.jsx
+++ src/Escena.jsx
@@ -20,6 +20,7 @@ export default function Escena({ ...props }) {
         <group ref={group} {...props} dispose={null}>
             <group ref={group} {...props} dispose={null}>
                 <mesh geometry={nodes.Cubo002.geometry} material={materials.piso} position={[-7., 0.1, -7.9]} scale={[2, 0.04, 2]}/> 
+                <p>hola mundo</p>
                 {/* <mesh geometry={nodes.piso.geometry} material={materials.piso} position={[-7, -0.9, -7]} scale={[2, 0.04, 4]}/> 
                 <mesh geometry={nodes.marcoSilla.geometry} material={materials.madera} position={[-4, 0, -7]} scale={[0.5, 0.05, 0.5 ]}/> 
                 <mesh geometry={nodes.coginSilla.geometry} material={materials.cogin} position={[-4, 0.11, -7]} scale={[0.5, 0.05, 0.5 ]}/>  */}
