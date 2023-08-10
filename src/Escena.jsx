9b3056f21bd41dafca5531296c8f3681f63ed183
DanielAros <danielaros200@gmail.com>
2023-08-10 13:08:46 -0600

    haciendo mi chamba x2

--- src/Escena.jsx
+++ src/Escena.jsx
@@ -8,7 +8,7 @@ import { useGLTF, CubeCamera } from '@react-three/drei'
 
 export default function Escena({ ...props }) {
     const group = useRef()
-    const { nodes, materials } = useGLTF('/escena.glb')
+    const { nodes, materials } = useGLTF('/escena2.glb')
     console.log({nodes,materials})
     // const snap = useSnapshot(state);
 
@@ -19,13 +19,13 @@ export default function Escena({ ...props }) {
     return (
         <group ref={group} {...props} dispose={null}>
             <group ref={group} {...props} dispose={null}>
-                <mesh geometry={nodes.Mesa.geometry} material={materials.madera} position={[-7., 0.1, -7.9]} scale={[2, 0.04, 2]}/> 
-                <mesh geometry={nodes.piso.geometry} material={materials.piso} position={[-7, -0.9, -7]} scale={[2, 0.04, 4]}/> 
+                <mesh geometry={nodes.Cubo002.geometry} material={materials.piso} position={[-7., 0.1, -7.9]} scale={[2, 0.04, 2]}/> 
+                {/* <mesh geometry={nodes.piso.geometry} material={materials.piso} position={[-7, -0.9, -7]} scale={[2, 0.04, 4]}/> 
                 <mesh geometry={nodes.marcoSilla.geometry} material={materials.madera} position={[-4, 0, -7]} scale={[0.5, 0.05, 0.5 ]}/> 
-                <mesh geometry={nodes.coginSilla.geometry} material={materials.cogin} position={[-4, 0.11, -7]} scale={[0.5, 0.05, 0.5 ]}/> 
+                <mesh geometry={nodes.coginSilla.geometry} material={materials.cogin} position={[-4, 0.11, -7]} scale={[0.5, 0.05, 0.5 ]}/>  */}
             </group>
         </group>
     )
 }
 
-useGLTF.preload('/escena.glb')
+useGLTF.preload('/escena2.glb')
