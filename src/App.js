0c5eba6ba0bcb34dbeb5c8ef193366d3207e69c3
Daniel Leon Paulin <dleon29@alumnos.uaq.mx>
2023-08-18 14:06:22 -0600

    Modelo 3d Sala

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { Environment, ContactShadows, OrbitControls, PerspectiveCamera } from '@react-three/drei'
-import Car from './Car'
-import Prueba from './Prueba'
-import Silla from './Silla'
-import Escena from './Escena'
+// import Car from './Car'
+import ModelViewer from './ModelViewer'
+
+
+// DESCARGAR MODELOS DE https://drive.google.com/drive/folders/1654D1Dti8cekUzB5c5rpE4EMnRr2V9Ko?usp=sharing
+// PONERLOS EN public/models/
+
 
 export default function App() {
   return (
@@ -13,11 +16,11 @@ export default function App() {
         <Environment files="/old_depot_2k.hdr" ground={{ height: 32, radius: 130 }} />
         <spotLight angle={1} position={[-80, 200, -100]} intensity={1} />
         {/* <Car position={[-8, 0, -2]} scale={20} rotation-y={-Math.PI / 4} /> */}
-        <Escena position={[-8, 0, -2]} scale={5} rotation-y={-Math.PI / 4}/>
+        <ModelViewer />
         <ContactShadows renderOrder={2} frames={1} resolution={1024} scale={120} blur={2} opacity={0.6} far={100} />
       </Suspense>
       <OrbitControls enableZoom={false} enablePan={false} minPolarAngle={0} maxPolarAngle={Math.PI / 2.25} makeDefault />
-      <PerspectiveCamera makeDefault position={[-30, 100, 120]} fov={35} />
+      <PerspectiveCamera makeDefault position={[-30, 50, 120]} fov={35} />
     </Canvas>
   )
 }
